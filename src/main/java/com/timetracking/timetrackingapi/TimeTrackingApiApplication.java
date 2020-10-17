4c3f7697c3aaeeb512136d6828b67081978834df
Angleby Vieira <angleby@gmail.com>
2020-10-17 13:50:59 -0300

    :tada: Commit inicial

---
+++ src/main/java/com/timetracking/timetrackingapi/TimeTrackingApiApplication.java
@@ -0,0 +1,13 @@
+package com.timetracking.timetrackingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeTrackingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeTrackingApiApplication.class, args);
+	}
+
+}
