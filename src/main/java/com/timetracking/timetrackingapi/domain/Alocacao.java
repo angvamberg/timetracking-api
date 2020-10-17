d3ff746a34bfea85398a2d32f8c5451e8ef2ebf4
Angleby Vieira <angleby@gmail.com>
2020-10-17 15:31:35 -0300

    :card_file_box: Mapear entidade Alocacao

---
+++ src/main/java/com/timetracking/timetrackingapi/domain/Alocacao.java
@@ -0,0 +1,38 @@
+package com.timetracking.timetrackingapi.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(schema = "TIMETRACKING", name = "ALOCACAO")
+public class Alocacao {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "CD_ALOCACAO")
+    private Long id;
+
+    @Column(name = "HORAS_ALOCADAS")
+    private int horasAlocadas;
+
+    @Column(name = "DIA_ALOCACAO")
+    private LocalDate diaAlocacao;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "CD_USUARIO")
+    private Usuario usuario;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "CD_PROJETO")
+    private Projeto projeto;
+
+}
